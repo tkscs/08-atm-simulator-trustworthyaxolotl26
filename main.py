3355f3acf7d3365808108fe2c61bde35c7b9451d
trustworthyaxolotl26 <efreeman2029@kehillahstudent.org>
2025-11-07 11:54:52 -0800

    atm simulator working

--- main.py
+++ main.py
@@ -14,6 +14,38 @@ Options:
 - loop (with a while loop) until the user says "exit"
 """
 
-# start with 1 million dollars
-balance = 1000000
+# start with dollars
+balance = 100
 
+def withdraw():
+    global balance
+    robbery = int(input("how much money you taking: "))
+    if robbery < 0: 
+        print("yeah, no. not how this works")
+    else:
+        balance = balance - robbery
+        print(f"you sucsessfully took ${robbery}")
+
+def deposit():
+    global balance
+    amount = int(input("how much are you depositing: "))
+    if amount < 0: 
+        print("you can just withdraw you know")
+    else:
+        balance = balance + amount
+        print(f"you sucsessfully depositied ${amount}")
+
+def check():
+    print(f"your balance is ${balance}")
+
+while True: 
+    print()
+    start = input("would you like to check balance, withdraw money, deposit money, or quit: ")
+    if start == "check balance":
+        check()
+    elif start == "withdraw money":
+        withdraw()
+    elif start == "deposit money":
+        deposit()
+    elif start == "quit":
+        break
